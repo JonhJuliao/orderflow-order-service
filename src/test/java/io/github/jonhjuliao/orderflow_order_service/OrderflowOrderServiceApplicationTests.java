fea09af44ee6c7b7d69b4a5a09f5d9576fdc0f2e
Jonathan Julião <jon.m.juliao@gmail.com>
2026-02-04 13:00:08 -0300

    test(db): configurar H2 e profile de teste para subir o contexto

--- src/test/java/io/github/jonhjuliao/orderflow_order_service/OrderflowOrderServiceApplicationTests.java
+++ src/test/java/io/github/jonhjuliao/orderflow_order_service/OrderflowOrderServiceApplicationTests.java
@@ -2,7 +2,9 @@ package io.github.jonhjuliao.orderflow_order_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class OrderflowOrderServiceApplicationTests {
 
