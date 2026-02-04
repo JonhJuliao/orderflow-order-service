3144fad12af58bde55e989bf25f3d211a74e4602
Jonathan Julião <jon.m.juliao@gmail.com>
2026-02-04 12:29:54 -0300

    chore(init): criar projeto base Spring Boot com Maven Wrapper

---
+++ src/main/java/io/github/jonhjuliao/orderflow_order_service/OrderflowOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.jonhjuliao.orderflow_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderflowOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jonhjuliao/orderflow_order_service/OrderflowOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonhjuliao.orderflow_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderflowOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
