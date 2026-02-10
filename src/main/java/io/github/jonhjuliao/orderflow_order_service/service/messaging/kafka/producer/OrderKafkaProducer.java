4deb33b027f8523afa6f33311540991ea631d2e6
Jonathan Julião <jon.m.juliao@gmail.com>
2026-02-10 16:20:25 -0300

    test(messaging): adiciona testes do listener de eventos kafka de pedidos

--- src/main/java/io/github/jonhjuliao/orderflow_order_service/service/messaging/kafka/producer/OrderKafkaProducer.java
+++ src/main/java/io/github/jonhjuliao/orderflow_order_service/service/messaging/kafka/producer/OrderKafkaProducer.java
@@ -1,5 +1,7 @@
 package io.github.jonhjuliao.orderflow_order_service.service.messaging.kafka.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -7,12 +9,20 @@ import org.springframework.stereotype.Component;
 public class OrderKafkaProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    private Logger log = LoggerFactory.getLogger(OrderKafkaProducer.class);
 
     public OrderKafkaProducer(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void publish(String topic, String key, Object payload) {
-        kafkaTemplate.send(topic, key, payload);
+        kafkaTemplate.send(topic, key, payload)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        log.error("Falha ao publicar evento no Kafka. topic={}, key={}", topic, key, ex);
+                    } else {
+                        log.debug("Evento publicado no Kafka. topic={}, key={}", topic, key);
+                    }
+                });
     }
 }
